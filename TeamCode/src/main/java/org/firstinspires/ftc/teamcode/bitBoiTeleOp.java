31e035b2863f900cbd7703865fd7865305bd5db1
evandehlinger <evandehlinger@foresthills.edu>
2017-10-24 15:01:41 -0400

    Added Bit Boiiiiiiiiiii

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bitBoiTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bitBoiTeleOp.java
@@ -24,7 +24,7 @@ public class bitBoiTeleOp extends OpMode{
     public void loop() {
 
         motor1.setPower((gamepad1.right_stick_y/3)-(2*gamepad1.right_stick_x/3));
-        motor2.setPower((gamepad1.right_stick_y/3)+(2*gamepad1.right_stick_x/3));
+        motor2.setPower((gamepad1.right_stick_y/3)-(2*gamepad1.right_stick_x/3));
 
     }
 }
