a0fab61fd4882ad71ca77135f149f3cc02145c04
evandehlinger <evandehlinger@foresthills.edu>
2017-10-24 10:13:09 -0400

    Added Bit Boiiiiiiiiiii

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bitBoiTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bitBoiTeleOp.java
@@ -1,40 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.GyroSensor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 
 /**
  * Created by evandehlinger on 10/24/17.
  */
-
-@Autonomous(name="Bit Boi Audio", group="Bit Boi")
+@TeleOp(name = "TeleOp Mode", group = "Bit Boi")
 //@Disabled
-public class bitBoi extends LinearOpMode {
-
+public class bitBoiTeleOp extends OpMode{
     DcMotor motor1;
     DcMotor motor2;
 
-    public void runOpMode() throws InterruptedException{
+    public void init() {
         motor1=hardwareMap.dcMotor.get("motor_1");
         motor2=hardwareMap.dcMotor.get("motor_2");
 
         motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
 
-        waitForStart();
-
-        motor1.setPower(.5);
-        motor2.setPower(.5);
-
-        sleep(10000);
-
+    public void loop() {
+        int x =1;
+        int y = 1;
+        if(gamepad1.left_stick_y<0){
+            x=-1;
+        }
+        if(gamepad1.right_stick_y<0){
+            y=-1;
+        }
+        motor1.setPower(Math.pow(y*gamepad1.left_stick_y,2));
+        motor2.setPower(Math.pow(x*gamepad1.right_stick_y,2));
 
     }
 }
