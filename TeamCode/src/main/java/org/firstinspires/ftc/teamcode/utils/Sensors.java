993f9553cc5b9f35e8ffbf7371e9616b79961381
theonlyones <anthonysalvato@foresthills.edu>
2017-11-12 11:41:50 -0500

    Enabled gyro test programs, removed errors from sensors.java sensors.java was rereferenced to oldSensors.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Sensors.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.utils;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -11,6 +12,10 @@ import com.qualcomm.robotcore.hardware.configuration.ExpansionHubMotorController
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import static org.firstinspires.ftc.teamcode.utils.oldSensors.gyro;
 
@@ -19,6 +24,7 @@ public class Sensors {
 
     public static double gyrochange;
     private static double timeAutonomous;
+    private static ElapsedTime gyrotime = new ElapsedTime();
     private static ElapsedTime runtime = new ElapsedTime();
     public static int gyroInitial;
     //public static ColorSensor leye;
@@ -27,7 +33,7 @@ public class Sensors {
     static boolean red;
     static  double trueHeading;
     public static double driverOffset = 0;
-
+    static Orientation angles;
     private static LinearOpMode opMode;
     private static Telemetry telemetry;
 
@@ -52,6 +58,12 @@ public class Sensors {
         telemetry = opMode.telemetry;
 
         BNO055IMU.Parameters IMUparams = new BNO055IMU.Parameters();
+        IMUparams.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        IMUparams.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        IMUparams.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        IMUparams.loggingEnabled      = true;
+        IMUparams.loggingTag          = "IMU";
+        IMUparams.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
         //reye = hardware_map.get(ColorSensor.class, "reye");
         //reye.setI2cAddress(I2cAddr.create8bit(0x5c));
         //reye.enableLed(false);
@@ -72,13 +84,27 @@ public class Sensors {
         gyroInitial = gyro.getHeading();
         */
         gyro.initialize(IMUparams);
+        telemetry.addData("IMUparams", IMUparams);
+        angles   = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+        telemetry.addData("angle", angles);
+
 
 
     }
 
+    public static Orientation readGyro()
+    {
+        if(gyrotime.milliseconds() >= 10)
+        {
+            gyrotime.reset();
+            angles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+        }
+        return angles;
+    }
+
 
     public static void gyroDriftRead() {
-
+/*
         if(gyro.getHeading() == 0) {
             gyrochange= 0;
         } else if (gyro.getHeading() < (gyroInitial + 180) - 360) {
@@ -88,14 +114,17 @@ public class Sensors {
         } else {
             gyrochange = (gyroInitial - gyro.getHeading()) / runtime.seconds();
         }
+        */
     }
 
     public static double gyroIntegratedHeading() {
-        return (gyrochange * (runtime.seconds())) + gyro.getIntegratedZValue();
+        //return (gyrochange * (runtime.seconds())) + gyro.getIntegratedZValue();
+        return 0;
     }
 
     public static double gyroHeading()
     {
+        /*
         //calculate modified heading
         trueHeading = (gyrochange * runtime.seconds()) + gyro.getHeading();
 
@@ -110,19 +139,23 @@ public class Sensors {
         }
         //if it is, then return
         return trueHeading;
+        */
+        return 0;
     }
 
     public static void resetGyro()
     {
-        gyro.resetZAxisIntegrator();
-        runtime.reset();
+        //gyro.resetZAxisIntegrator();
+        //runtime.reset();
     }
 
     public static void offsetReset() {
+        /*
         telemetry.addData(">", "gyro resetting");
         telemetry.update();
         gyro.calibrate();
         gyrochange = 0;
+        */
     }
 
 
